632fb6c2237b12c19d8d2df621b4fa7646cae43b
Ken <ken.valverde@yahoo.com>
2021-12-22 17:01:37 -0600

    Create Customer, Category, FoodItems, and UserFoodItem models.

--- CalorieCalculator/FoodGram/models.py
+++ CalorieCalculator/FoodGram/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email= models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return str(self.name)
+
+class Category(models.Model):
+    options = (
+        ('breakfast', 'breakfast'),
+        ('lunch', 'lunch'),
+        ('dinner', 'dinner'),
+        ('snacks', 'snacks'),
+    )
+    name = models.CharField(max_length=50, choices=options)
+    
+    def __str__(self):
+        return self.name
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ManyToManyField(Category)
+    carbohydrate = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    fats = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    protein = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    calorie = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    quantity = models.IntegerField(default=1, null=True, blank=True)
+
+# For user page ---------------------------------------------------------------------------
+class UserFoodItem(models.Model):
+    customer = models.ManyToManyField(Customer, blank=True)
+    fooditem = models.ManyToManyField(FoodItem) 
